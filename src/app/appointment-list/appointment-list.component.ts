0206bf1506cc8040eab198118d8b5123e6367122
sahuji90 <sahusunil90@gmail.com>
2024-07-13 20:46:29 +0530

    updated addAppointment method to add data from html into appointment array

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -12,6 +12,16 @@ export class AppointmentListComponent {
   appointment: Appointment [] = [];
 
   addAppointment() {
-    alert(this.newAppointmentTitle + ' ' + this.newAppointmentDate);
+    if(this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
+      let newAppointment: Appointment = {
+        id: Date.now(),
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate
+      }
+      this.appointment.push(newAppointment);
+      this.newAppointmentTitle = '';
+      this.newAppointmentDate = new Date();
+      alert(this.appointment.length)
+    }
   }
 }
